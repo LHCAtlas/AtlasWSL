bfa848849d3b4d67519e834427a42a6254eb8a83
Gordon Watts <gordon@gordonandpaula.net>
2016-11-26 11:41:05 -0800

    Fix up some mistakes with how we were building root
    Not really understanding cmake and the like. :-)

--- compile_ROOT.sh
+++ compile_ROOT.sh
@@ -10,16 +10,18 @@ tag=v6-04-16
 # Get git down here and clone it.
 if [ ! -d ~/ATLAS/root-source ]; then
   git clone http://root.cern.ch/git/root.git ~/ATLAS/root-source/root
-  cd ~/ATLAS/root-source
-  git checkout -b $tag $tag
 fi
-loc=~/ATLAS/root-source
+cd ~/ATLAS/root-source/root
+
+# Has the branch been checked out?
+git checkout -B $tag $tag
+loc=~/ATLAS/root-source/$tag
 
 # Configure and build with cmake.
 # Instructions found here: https://root.cern.ch/building-root
 
-mkdir $tag
-cd $tag
-cmake $loc
+mkdir -p $loc
+cd $loc
+cmake ~/ATLAS/root-source/root
 cmake --build . -- -j2
 
