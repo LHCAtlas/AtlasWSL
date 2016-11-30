53bd1d81f1bc800cd95b1d60398e391b30bd9367
Gordon Watts <gordon@gordonandpaula.net>
2016-11-29 20:15:43 -0800

    First go at a full driver for the CI
    Goal is to have it run everything and Jenkins just invoke it. This is a
    start...

---
+++ CIBuilder/ci_driver.bat
@@ -0,0 +1,14 @@
+REM Overall driver for continuous integration. It will blow away everything.
+REM WSL should already be enabled
+REM should be run from the root directory
+
+REM Remove and install the linux subsystem to make sure we are starting
+REM with a clean known state.
+REM This will install with root as the default username.
+
+lxrun /uninstall /full /y
+lxrun /install /y
+
+REM Next, configure the machine as sudo in order to have what is needed
+REM to run software (e.g. have gcc 4.9, etc.).
+bash setup_sudo.sh
