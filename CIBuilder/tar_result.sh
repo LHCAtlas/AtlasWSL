59f12e9f9db04d1a3fa03dd2406a3114e8185458
Gordon Watts <gordon@gordonandpaula.net>
2016-12-07 14:22:04 -0800

    Fix up script to run

--- CIBuilder/tar_result.sh
+++ CIBuilder/tar_result.sh
@@ -2,8 +2,8 @@
 #
 #  Save the result as a TAR file.
 #
-$stub=$1
+stub=$1
 
 cd ~
 lxrel=`lsb_release --release --short`
-tar -czf AnalysisBase-$stub-ubuntu-$lxrel.tar.gz ATLAS"
+tar -czf AnalysisBase-$stub-ubuntu-$lxrel.tar.gz ATLAS
