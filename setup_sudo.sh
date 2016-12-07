9aa5351c5fb6ea0909df9e44c73fd604b63e188b
Gordon Watts <gordon@gordonandpaula.net>
2016-12-06 22:28:39 -0800

    Split installs onto seperate lines
    Some of them seem to get missed... otherwise...

--- setup_sudo.sh
+++ setup_sudo.sh
@@ -32,17 +32,45 @@ fi
 apt-get install -y krb5-user
 
 # Compilers
-apt-get install -y build-essential checkinstall gcc-4.9 g++-4.9
+apt-get install -y build-essential
+apt-get install -y checkinstall
+apt-get install -y gcc-4.9
+apt-get install -y g++-4.9
 
 # ROOT packages needed for building
 
-apt-get install -y dpkg-dev make g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev cmake
+apt-get install -y dpkg-dev
+apt-get install -y make
+apt-get install -y g++
+apt-get install -y gcc
+apt-get install -y binutils
+apt-get install -y libx11-dev
+apt-get install -y libxpm-dev
+apt-get install -y libxft-dev
+apt-get install -y libxext-dev
+apt-get install -y cmake
 
-apt-get install -y libssl-dev libpcre3-dev xlibmesa-glu-dev libglew1.5-dev libftgl-dev libmysqlclient-dev libfftw3-dev cfitsio-dev \
-graphviz-dev libavahi-compat-libdnssd-dev libldap2-dev python python-dev libxml2-dev libkrb5-dev libgsl0-dev libqt4-dev
+apt-get install -y python
+apt-get install -y libssl-dev
+apt-get install -y libpcre3-dev
+apt-get install -y xlibmesa-glu-dev
+apt-get install -y libglew1.5-dev
+apt-get install -y libftgl-dev
+apt-get install -y libmysqlclient-dev
+apt-get install -y libfftw3-dev
+apt-get install -y cfitsio-dev
+apt-get install -y graphviz-dev
+apt-get install -y libavahi-compat-libdnssd-dev
+apt-get install -y libldap2-dev
+apt-get install -y python-dev
+apt-get install -y libxml2-dev
+apt-get install -y libkrb5-dev
+apt-get install -y libgsl0-dev libqt4-dev
 
 # CERN Access. Unfortunately, this will make a demand of the user
-apt-get install -y subversion krb5-config krb5-user
+apt-get install -y subversion
+apt-get install -y krb5-config
+apt-get install -y krb5-user
 
 # Required for RootCore builds.
 apt-get install -y libboost1.55-all-dev
